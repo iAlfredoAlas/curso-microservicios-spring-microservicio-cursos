697ece8818f243b66dadeddd2493a49ab7c00842
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-23 23:18:23 -0600

    refactor adding dependency for .env

--- src/main/java/com/curso/microservicios/spring/cursos/CursoMicroserviciosSpringMicroservicioCursosApplication.java
+++ src/main/java/com/curso/microservicios/spring/cursos/CursoMicroserviciosSpringMicroservicioCursosApplication.java
@@ -3,12 +3,17 @@ package com.curso.microservicios.spring.cursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class CursoMicroserviciosSpringMicroservicioCursosApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(CursoMicroserviciosSpringMicroservicioCursosApplication.class, args);
 	}
 
