26193b24a1b35b570b972dd98eb7547ffe6f337a
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-23 23:18:44 -0600

    add CursoService.java

---
+++ src/main/java/com/curso/microservicios/spring/cursos/services/CursoService.java
@@ -0,0 +1,8 @@
+package com.curso.microservicios.spring.cursos.services;
+
+import com.curso.microservicios.spring.commons.services.CommonService;
+import com.curso.microservicios.spring.cursos.models.entity.Curso;
+
+public interface CursoService extends CommonService<Curso> {
+
+}
