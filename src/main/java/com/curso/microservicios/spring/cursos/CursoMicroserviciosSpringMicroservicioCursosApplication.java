cf34f16ea14c11465e8faec2d2f509c66be3053f
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-24 00:31:09 -0600

    refactor @EntityScan

--- src/main/java/com/curso/microservicios/spring/cursos/CursoMicroserviciosSpringMicroservicioCursosApplication.java
+++ src/main/java/com/curso/microservicios/spring/cursos/CursoMicroserviciosSpringMicroservicioCursosApplication.java
@@ -2,11 +2,14 @@ package com.curso.microservicios.spring.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.curso.microservicios.spring.app.generic.alumno.models.entity",
+	"com.curso.microservicios.spring.cursos.models.entity"})
 public class CursoMicroserviciosSpringMicroservicioCursosApplication {
 
 	public static void main(String[] args) {
