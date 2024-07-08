6f615299eacfb2aa1517259773a59417ba316ff7
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-08 11:21:36 -0600

    add methods for alumnos and add alumnos relations

--- src/main/java/com/curso/microservicios/spring/cursos/models/entity/Curso.java
+++ src/main/java/com/curso/microservicios/spring/cursos/models/entity/Curso.java
@@ -1,35 +1,48 @@
 package com.curso.microservicios.spring.cursos.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
+
 @Entity
 @Table(name = "cursos")
 public class Curso {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "nombre")
 	private String nombre;
-	
+
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
-	
+
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Alumno> alumnos;
+
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
+	}
+
+	public Curso() {
+		this.alumnos = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -55,7 +68,21 @@ public class Curso {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public void addAlumnos(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
 	
-	
-	
+	public void removeAlumnos(Alumno alumno) {
+		this.alumnos.remove(alumno);
+	}
+
 }
