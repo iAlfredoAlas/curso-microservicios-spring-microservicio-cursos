d55c8bfa341ce23579a5ff644d03d43dc0ae3e88
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-24 15:00:11 -0600

    add @Valid in editar endpoint

--- src/main/java/com/curso/microservicios/spring/cursos/controllers/CursoController.java
+++ src/main/java/com/curso/microservicios/spring/cursos/controllers/CursoController.java
@@ -3,8 +3,11 @@ package com.curso.microservicios.spring.cursos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,7 +24,10 @@ import com.curso.microservicios.spring.generic.examenes.models.entity.Examen;
 public class CursoController extends CommonController<Curso, CursoService> {
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+	public ResponseEntity<?> editar(@Valid @RequestBody Curso curso, BindingResult result, @PathVariable Long id) {
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
 		Optional<Curso> optCurso = this.service.findbyId(id);
 		if (optCurso.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
