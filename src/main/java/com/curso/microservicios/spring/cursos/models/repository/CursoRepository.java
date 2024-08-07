c429ec2f4335d8223fd2772eb4451564954d3fb0
Alfredo Alas <alfredo.alas14@gmail.com>
2024-08-07 10:09:18 -0600

    change extends to PagingAndSortingRepository

--- src/main/java/com/curso/microservicios/spring/cursos/models/repository/CursoRepository.java
+++ src/main/java/com/curso/microservicios/spring/cursos/models/repository/CursoRepository.java
@@ -1,11 +1,11 @@
 package com.curso.microservicios.spring.cursos.models.repository;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.curso.microservicios.spring.cursos.models.entity.Curso;
 
-public interface CursoRepository extends CrudRepository<Curso, Long> {
+public interface CursoRepository extends PagingAndSortingRepository<Curso, Long> {
 
 	@Query("select cur from Curso cur join fetch cur.alumnos alu where alu.id=?1")
 	public Curso findCursoByAlumnoId(Long id);
